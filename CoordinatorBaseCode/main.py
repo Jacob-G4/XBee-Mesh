c5a720fea008a9ae78684a86f5bb880935b86945
JacobG <jacobgarrity4@gmail.com>
2025-09-17 14:13:47 -0400

    Bug fixes and improvements
    In this push example code was fixed to remove unnecessary test code; Fix bug with message processing; Provide examples of correct usage; Add comments and warnings.

--- CoordinatorBaseCode/main.py
+++ CoordinatorBaseCode/main.py
@@ -5,29 +5,43 @@ from machine import Pin
 import time #Use board clock for timing
 
 #####-----Pins----#####
+# Use this space to declare all pins in and out of the device
 #D0 -> DIO0
 #D1 -> DIO1
 #D2 -> DIO2
 #D3 -> DIO3
 #D4 -> DIO4
-Iridium0 = Pin(machine.Pin.board.D0, Pin.IN)
-Iridium1 = Pin(machine.Pin.board.D1, Pin.IN)
-Iridium2 = Pin(machine.Pin.board.D2, Pin.IN)
+
+#Example of how to use Pin's to read from Iridium
+#Iridium0 = Pin(machine.Pin.board.D0, Pin.IN)
+#Iridium1 = Pin(machine.Pin.board.D1, Pin.IN)
+#Iridium2 = Pin(machine.Pin.board.D2, Pin.IN)
+
+
+
+
 
 #####-----Global Variables-----#####
-name = '[COORDINATOR NAME]'
-iterations = 0
-networkConnections = 0
+# Use this space to house all GLOBAL Variables
+name = '[COORDINATOR NAME]' #Name to be sent along with all messages for datalogging purposes
 
-#####-----Methods-----#####
-#Method used to get a message from the network. Runs every iteration of the main loop
+#####-----'Back end' Methods-----#####
+# Space for code that is specifically used to send and recieve messages from
+# other parts of the network. Do not modify this code without backing up data
+# and ensuring that you know what you are doing
+
+
+# Method used to get a message from the network. Runs every iteration of the main loop
+# Do not modify without cause
 def getMessage():
     message = xbee.receive()
     if message:
-        print(message)
-        return message.get('payload').decode('utf-8')
+        print(message) # Debug print only seen in console
+        return eval(message.get('payload').decode('utf-8'))
 
-#Method to send an exact string to the entire network. Includes error handling for too many consecutive messages
+# Method to send an exact string to the entire network. Includes error
+# handling for too many consecutive messages
+# Do not modify OR call without cause
 def sendExactMessage(message = ''):
     try:
         xbee.transmit(xbee.ADDR_BROADCAST, message)
@@ -36,25 +50,48 @@ def sendExactMessage(message = ''):
         time.sleep(0.3)
         sendExactMessage(message)
 
-#Method to send a message out to the entire network after formatting
+# Method to send a message out to the entire network after formatting
+# Do not modify without cause
 def sendMessage(message = ''):
-    message = "{'from':'"+name+"','msg':"+message+"}"
+    message = "{'from':'" + name + "','msg':" + message + "}"
     sendExactMessage(message)
 
 
+
+
+
 #####-----SPACE FOR GENERAL USERS-----#####
-#Method to allow router to complete tasks, limited to every second to save processing power
+# Space for User to define all needed methods and use the provided methods to
+# call them for use when timing permits it. All code developed by users and not
+# directly pertaining to the operation of the XBee itself should be put here
+
+
+# Method to allow router to complete tasks, limited to every second to save
+# processing power
+# DO NOT INCLUDE PYTHON CODE IN MESSAGES
 def proactiveSpace():
-    sendMessage("{'temp':'25','vel':'32'}")
-    print('Sending')
+#    sendMessage("{'temp':'25','vel':'32'}") # Example of how to send data that can be proccessed on other routers in the network
 
-#Method to allow router reactions to incoming messages, activates every time a message is recieved
+    print('Proactive Space') # Debug print
+
+# Method to allow router reactions to incoming messages, activates every time a
+# message is recieved
 def reactiveSpace(message):
-    print(message)
+# Example of how to process incoming messages within the reactive space
+#    if(message.get('from') == '[COORDINATOR NAME]'): # Check where the message is coming from
+#         message = message.get('msg') # Strip the sender and just use the actual message
+#         message = dict(message) # Convert the message into a dictionary for easier processing
+#         if(message.get('temp')): # Check if the information you want is avilible in the message
+#            print('The Temperature is ' + message.get('temp')) # Print the information (Or act on it through another method)
 
-def readIridium():
-    State = (1 * Iridium0.value()) + (2 * Iridium1.value()) + (4 * Iridium2.value())
-    sendMessage(str(State))
+    print(message) # Debug print
+
+# Example method to read data from iridium Pins
+#def readIridium():
+    #Sum the binary code of Iridium to determine code number
+    #State = (1 * Iridium0.value()) + (2 * Iridium1.value()) + (4 * Iridium2.value())
+    #Send that code to the network as a message
+    #sendMessage(str(State))
 
 
 
@@ -65,20 +102,27 @@ def readIridium():
 
 
 #####-----Main Loop-----#####
+# The main code to be run on the device. This loop includes specific timings
+# to ensure device does not crash due to overflows or extra usage. Do not
+# modify this code without backing up data and ensuring that you know what you
+# are doing
+
+iterations = 0 # Flag to keep track of loop iterations
 while True:
     #Check for a new message in the stack
     message = getMessage()
 
     #If there is a new message react to it
     if message:
-        reactiveSpace(message)
+        reactiveSpace(message) # Call the user defined method
 
-    #Every 5 seconds complete actions on board
+    # Every 5 Iteration(s) complete actions in proactive space and reset the
+    # Iterations counter to zero
     if iterations % 5 == 0:
         proactiveSpace()
         iterations = 0
 
-    #Slight buffer to save resources
+    # Slight buffer to save resources
     time.sleep(0.2)
     iterations += 1
 
--- RouterBaseCode/main.py
+++ RouterBaseCode/main.py
@@ -5,32 +5,37 @@ from machine import Pin
 import time #Use board clock for timing
 
 #####-----Pins----#####
+# Use this space to declare all pins in and out of the device
 #D0 -> DIO0
 #D1 -> DIO1
 #D2 -> DIO2
 #D3 -> DIO3
 #D4 -> DIO4
-Iridium0 = Pin(machine.Pin.board.D0, Pin.OUT, value = 0)
-Iridium1 = Pin(machine.Pin.board.D1, Pin.OUT, value = 0)
-Iridium2 = Pin(machine.Pin.board.D2, Pin.OUT, value = 0)
-#GPIOtest2 = Pin(machine.Pin.board.D1, Pin.OUT, value = 0)
-j = 0
 
 
-#####-----Static Information-----#####
+
+#####-----Global Variables-----#####
+# Use this space to house all GLOBAL Variables
 name = '[ROUTER NAME]' #Name to be sent along with all messages for datalogging purposes
 
-#####-----Methods-----#####
+#####-----'Back end' Methods-----#####
+# Space for code that is specifically used to send and recieve messages from
+# other parts of the network. Do not modify this code without backing up data
+# and ensuring that you know what you are doing
+
+
 #Method used to get a message from the network. Runs every iteration of the main loop
+# Do not modify without cause
 def getMessage():
     message = xbee.receive()
     if message:
-        print(message)  # Debug print only seen in console
-        return message.get('payload').decode('utf-8')
+        #print(message) # Debug print only seen in console
+        return eval(message.get('payload').decode('utf-8'))
 
-
-# Method to send an exact string to the entire network. Includes error handling for too many consecutive messages
-def sendExactMessage(message=''):
+# Method to send an exact string to the entire network. Includes error
+# handling for too many consecutive messages
+# Do not modify OR call without cause
+def sendExactMessage(message = ''):
     try:
         xbee.transmit(xbee.ADDR_BROADCAST, message)
     except OSError as e:
@@ -39,37 +44,42 @@ def sendExactMessage(message=''):
         sendExactMessage(message)
 
 # Method to send a message out to the entire network after formatting
-def sendMessage(message=''):
+# Do not modify without cause
+def sendMessage(message = ''):
     message = "{'from':'" + name + "','msg':'" + message + "'}"
     sendExactMessage(message)
 
 
+
+
+
 #####-----SPACE FOR GENERAL USERS-----#####
-#Method to allow router to complete tasks, limited to every second to save processing power
+# Space for User to define all needed methods and use the provided methods to
+# call them for use when timing permits it. All code developed by users and not
+# directly pertaining to the operation of the XBee itself should be put here
+
+
+# Method to allow router to complete tasks, limited to every second to save
+# processing power
+# DO NOT INCLUDE PYTHON CODE IN MESSAGES
 def proactiveSpace():
-    global j
-    if(j == 2):
-        GPIOtest1.value(1)
-        #GPIOtest2.value(1)
-        print('LEDs On')
-    if(j == 4):
-        GPIOtest1.value(0)
-        #GPIOtest2.value(0)
-        j=0
-        print('LEDs Off')
-    j+=1
+#    sendMessage("{'temp':'25','vel':'32'}") # Example of how to send data that can be proccessed on other routers in the network
 
-#Method to allow router reactions to incoming messages, activates every time a message is recieved
+    print('Proactive Space') # Debug print
+
+# Method to allow router reactions to incoming messages, activates every time a
+# message is recieved
 def reactiveSpace(message):
-    #if message.get('from') == '[COORDINATOR NAME]':
-       # message = message.get('msg')
-        #print(message.get('temp'))
-    print(message)
+# Example of how to process incoming messages within the reactive space
+#    if(message.get('from') == '[COORDINATOR NAME]'): # Check where the message is coming from
+#         message = message.get('msg') # Strip the sender and just use the actual message
+#         message = dict(message) # Convert the message into a dictionary for easier processing
+#         if(message.get('temp')): # Check if the information you want is avilible in the message
+#            print('The Temperature is ' + message.get('temp')) # Print the information (Or act on it through another method)
 
-    #if(message.get('from')=='[COORDINATOR NAME]'):
-        #message = message.get('msg')
-        #message = dict(message)
-        #print('The Temperature is'+message.get('temp'))
+    print(message) # Debug print
+
+
 
 
 
@@ -79,15 +89,26 @@ def reactiveSpace(message):
 
 
 #####-----Connect To Network-----#####
+# Code to allow the Xbee to connect to the Coordinator at the head of the
+# network Do not modify this code without backing up data and ensuring that you
+# know what you are doing
+
 print("Waiting for XBee to join network...")
 while xbee.atcmd('AI') != 0:
     time.sleep(0.1)
 print(name, " joined network.")
 sendMessage('Joined network')
 
+
 #####-----Main Loop-----#####
+# The main code to be run on the device. This loop includes specific timings
+# to ensure device does not crash due to overflows or extra usage. Do not
+# modify this code without backing up data and ensuring that you know what you
+# are doing
+
 iterations = 0
 while True:
+    # Check if connected to the Network
     if xbee.atcmd('AI') == 0:
 
         #Check for a new message in the stack
@@ -95,9 +116,10 @@ while True:
 
         #If there is a new message react to it
         if message:
-            reactiveSpace(message)
+            reactiveSpace(message) # Call the user defined method
 
-        #Every 5 seconds complete actions on board
+        # Every 5 Iteration(s) complete actions in proactive space and reset the
+        # Iterations counter to zero
         if iterations % 5 == 0:
             proactiveSpace()
             iterations = 0
